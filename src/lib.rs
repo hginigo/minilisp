04ba587141aebc0c41208b06a04f748145f19172
Hugo Iñigo <hinigo001@ikasle.ehu.eus>
2020-10-21 23:52:42 +0200

    Implemented trait TryFrom for type Atom

--- src/lib.rs
+++
@@ -1,1 +0,0 @@
-mod lisp_descr;
--- src/lisp_descr.rs
+++ src/lisp_descr.rs
@@ -1,23 +1,78 @@
 pub use either::Either;
+use std::convert::TryFrom;
+use std::string;
 
 // Basic types
-pub type LispType = Either<Atom, List>;
+pub type LispValue = Either<Expression, List>;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Atom {
     Bool(bool),
     // NOTE: float support needed
     Number(i32),
     Char(char),
-    String(String),
+    // NOTE: use str instead
+    String(string::String),
 }
 
-pub type List = Vec<Atom>;
+impl TryFrom<&str> for Atom {
+//impl Atom {
+    // Try to convert the given str into an Atom
+    type Error = &'static str;
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        // Bool
+        if s.eq("#t") {
+            return Ok(Atom::Bool(true));
+        } else if s.eq("#f") {
+            return Ok(Atom::Bool(true));
+        }
+
+        // Number
+        match s.parse::<i32>() {
+            Ok(num) => return Ok(Atom::Number(num)),
+            _ => {}
+        }
+
+        // Char and String
+        if s.len() < 2 {
+            return Err("Unknown identifier.");
+        }
+
+        let mut s = String::from(s);
+        //println!("s: {}", s);
+
+        let la = s.pop().expect("Unexpected error occurred.");
+        let fi = s.remove(0);
+        //println!("fi: {}, s: {}, la: {}", fi, s, la);
+
+        if !la.eq(&fi) {
+            return Err("String or Char not closed correctly.");
+        }
+
+        match fi {
+            '"' => Ok(Atom::String(s)),
+            '\'' => {
+                if s.len() > 1 {
+                    return Err("Too many chars to parse.") 
+                }
+                match s.chars().next() {
+                    None => Err("Cannot parse empty char."),
+                    Some(c) => Ok(Atom::Char(c)),
+                }
+            }
+            _ => Err("Unknown identifier"),
+        }
+    }
+}
+
+pub type List = Vec<Expression>;
 
 // Expressions
 pub type Expression = Either<Atom, Compound>;
 
 pub struct Compound {
-    operator: Atom,
+    operator: String,
     operands: Vec<Expression>,
 }
 
@@ -25,5 +80,6 @@ pub struct Compound {
 // etc... TODO functions, lambda, let, define
 
 // Keywords
-pub const LispOp: char = '(';
-pub const LispCl: char = ')';
+pub const LISP_OPC: char = '(';
+pub const LISP_CLC: char = ')';
+pub const LISP_SEP: char = ' ';
